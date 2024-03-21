7ba72d3ed73f2a17d2e33e65ffd0699af1f55e6b
doruk.tekel <doruk.tekel@bilgeadam.com>
2024-03-21 15:26:03 +0300

    register/login pasges was done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import "./App.css";
-import { Link, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Layout from "./Layout";
 import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/login" element={<Login />}></Route>
       </Route>
     </Routes>
   );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="container">
+      <form action="" className="login">
+        <h2>Login</h2>
+        <input type="text" placeholder="Username" />
+        <input type="text" placeholder="Password" />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
